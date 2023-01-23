935a5140ec07c7be84281d3cc1a3a0cf1da38fa5
Alejandro Meza <alemz-li@outlook.com>
2023-01-23 09:34:49 -0800

    Awesome config cleanup

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -5,78 +5,97 @@ pcall(require, "luarocks.loader")
 -- Standard awesome library
 local gears = require("gears")
 local awful = require("awful")
-require("awful.autofocus")
--- Widget and layout library
-local wibox = require("wibox")
+
 -- Theme handling library
 local beautiful = require("beautiful")
--- Notification library
+
+-- Miscellanous awesome library
 local menubar = require("menubar")
-local hotkeys_popup = require("awful.hotkeys_popup")
--- Enable hotkeys help widget for VIM and other apps
--- when client with a matching name is opened:
-require("awful.hotkeys_popup.keys")
 
 RC = {} -- global namespace, on top before require any modules
 RC.vars = require("main.user-variables")
 
-modkey = RC.vars.modkey
-
+-- {{{ Error handling -- }}}
 require("main.error-handling")
 
+-- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+beautiful.wallpaper = RC.vars.wallpaper
+-- }}}
 
--- Custom local library
+modkey = RC.vars.modkey
+
+-- Custom Local Library
 local main = {
   layouts = require("main.layouts"),
   tags    = require("main.tags"),
   menu    = require("main.menu"),
-  rules   = require("main.rules")
+  rules   = require("main.rules"),
 }
 
--- Layouts
-RC.layouts = main.layouts()
-
--- Tags
-RC.tags = main.tags()
-
--- Menu
-RC.mainmenu = awful.menu({ items = main.menu() })
-RC.launcher = awful.widget.launcher(
-  { image = beautiful.awesome_icon, menu = RC.mainmenu }
-)
-menubar.utils.terminal = RC.vars.terminal
-
-
--- Menubar configuration
-menubar.utils.terminal = terminal -- Set the terminal for applications that require it
--- }}}
-
--- Keyboard map indicator and switcher
-mykeyboardlayout = awful.widget.keyboardlayout()
-
--- Statusbar
-require("deco.statusbar")
-
+-- Custom Local Library: Keys and Mouse Binding
 local binding = {
   globalbuttons = require("binding.globalbuttons"),
   clientbuttons = require("binding.clientbuttons"),
   globalkeys    = require("binding.globalkeys"),
-  clientkeys    = require("binding.clientkeys"),
-  bindtotags    = require("binding.bindtotags")
+  bindtotags    = require("binding.bindtotags"),
+  clientkeys    = require("binding.clientkeys")
 }
 
--- Mouse and Key bindings
+-- {{{ Layouts
+-- Table of layouts to cover with awful.layout.inc, order matters.
+-- a variable needed in main.tags, and statusbar
+-- awful.layout.layouts = { ... }
+RC.layouts = main.layouts()
+-- }}}
+
+-- {{{ Tags
+-- Define a tag table which hold all screen tags.
+-- a variable needed in rules, tasklist, and globalkeys
+RC.tags = main.tags()
+-- }}}
+
+-- {{{ Menu
+-- Create a laucher widget and a main menu
+RC.mainmenu = awful.menu({ items = main.menu() }) -- in globalkeys
+
+-- a variable needed in statusbar (helper)
+RC.launcher = awful.widget.launcher(
+  { image = beautiful.awesome_icon, menu = RC.mainmenu }
+)
+
+-- Menubar configuration
+-- Set the terminal for applications that require it
+menubar.utils.terminal = RC.vars.terminal
+
+-- }}}
+
+-- {{{ Mouse and Key bindings
 RC.globalkeys = binding.globalkeys()
 RC.globalkeys = binding.bindtotags(RC.globalkeys)
 
 -- Set root
 root.buttons(binding.globalbuttons())
 root.keys(RC.globalkeys)
+-- }}}
 
--- Rules
-awful.rules.rules = main.rules(binding.clientkeys(), binding.clientbuttons())
+-- Keyboard map indicator and switcher
+mykeyboardlayout = awful.widget.keyboardlayout()
 
--- Signals
+-- {{{ Statusbar: Wibar
+require("deco.statusbar")
+-- }}}
+
+-- {{{ Rules
+-- Rules to apply to new clients (through the "manage" signal).
+awful.rules.rules = main.rules(
+  binding.clientkeys(),
+  binding.clientbuttons()
+)
+-- }}}
+
+-- {{{ Signals
 -- require("main.signals")
+-- }}}
+
