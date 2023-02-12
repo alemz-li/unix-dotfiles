130410d601fd219aff2b32c3de2a1a5278f01eab
Alejandro Meza <alemz-li@outlook.com>
2023-02-12 10:10:26 -0800

    Changed null-ls config to make formating and linting work

--- .config/nvim/lua/alemz/plugins/lsp/null-ls.lua
+++ .config/nvim/lua/alemz/plugins/lsp/null-ls.lua
@@ -1,49 +1,53 @@
-local setup, null_ls = pcall(require, "null_ls")
-if not setup then
-  print("NO setup")
-  return
-end
+local null_ls = require("null-ls")
 
-print('setup!')
 local formatting = null_ls.builtins.formatting
+local code_actions = null_ls.builtins.code_actions
 local diagnostics = null_ls.builtins.diagnostics
 
--- to setup format on save
+local sources = {
+	--[[ formatting ]]
+	formatting.eslint_d,
+	formatting.autopep8,
+	formatting.stylua,
+	formatting.clang_format,
+	formatting.stylelint,
+	formatting.prettier,
+	formatting.phpcbf,
+
+	--[[ code actions ]]
+	code_actions.eslint_d,
+
+	--[[ diagnostics ]]
+	diagnostics.eslint_d,
+}
+
+local lsp_formatting = function(bufnr)
+	vim.lsp.buf.format({
+		filter = function(client)
+			-- apply whatever logic you want (in this example, we'll only use null-ls)
+			return client.name == "null-ls"
+		end,
+		bufnr = bufnr,
+	})
+end
+
+-- if you want to set up formatting on save, you can use this as a callback
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
+local on_attach = function(client, bufnr)
+	if client.supports_method("textDocument/formatting") then
+		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = augroup,
+			buffer = bufnr,
+			callback = function()
+				lsp_formatting(bufnr)
+			end,
+		})
+	end
+end
 
--- configure null_ls
 null_ls.setup({
-  -- setup formatters & linters
-  sources = {
-    --  to disable file types use
-    --  "formatting.prettier.with({disabled_filetypes: {}})" (see null-ls docs)
-    formatting.prettier, -- js/ts formatter
-    formatting.stylua, -- lua formatter
-    diagnostics.eslint_d.with({ -- js/ts linter
-      -- only enable eslint if root has .eslintrc.js (not in youtube nvim video)
-      condition = function(utils)
-        return utils.root_has_file(".eslintrc.js") -- change file extension if you use something else
-      end,
-    }),
-  },
-  -- configure format on save
-  on_attach = function(current_client, bufnr)
-    if current_client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format({
-            filter = function(client)
-              --  only use null-ls for formatting instead of lsp server
-              return client.name == "null-ls"
-            end,
-            bufnr = bufnr,
-          })
-        end,
-      })
-    end
-  end,
+	sources = sources,
+	on_attach = on_attach,
 })
