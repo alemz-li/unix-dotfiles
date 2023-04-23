17cf2bb92e0ec18c42e673283663be7a77e04370
Alejandro Meza <alemz-li@outlook.com>
2023-04-23 11:27:45 -0700

    Removed keyboard layout from statusbar

--- .config/awesome/deco/statusbar.lua
+++ .config/awesome/deco/statusbar.lua
@@ -1,18 +1,18 @@
 -- Standard awesome library
 local gears = require("gears")
-local awful     = require("awful")
+local awful = require("awful")
 
 -- Wibox handling library
 local wibox = require("wibox")
 
 -- Custom Local Library: Common Functional Decoration
 local deco = {
-  wallpaper = require("deco.wallpaper"),
-  taglist   = require("deco.taglist"),
-  tasklist  = require("deco.tasklist")
+	wallpaper = require("deco.wallpaper"),
+	taglist = require("deco.taglist"),
+	tasklist = require("deco.tasklist"),
 }
 
-local taglist_buttons  = deco.taglist()
+local taglist_buttons = deco.taglist()
 local tasklist_buttons = deco.tasklist()
 
 local _M = {}
@@ -21,106 +21,113 @@ local _M = {}
 
 -- {{{ Wibar
 -- Create a textclock widget
-mytextclock = wibox.widget.textclock()
+local mytextclock = wibox.widget.textclock()
 
 awful.screen.connect_for_each_screen(function(s)
-  -- Wallpaper
-  set_wallpaper(s)
+	-- Wallpaper
+	set_wallpaper(s)
 
-  -- Create a promptbox for each screen
-  s.mypromptbox = awful.widget.prompt()
+	-- Create a promptbox for each screen
+	s.mypromptbox = awful.widget.prompt()
 
-  -- Create an imagebox widget which will contain an icon indicating which layout we're using.
-  -- We need one layoutbox per screen.
-  s.mylayoutbox = awful.widget.layoutbox(s)
-  s.mylayoutbox:buttons(gears.table.join(
-    awful.button({ }, 1, function () awful.layout.inc( 1) end),
-    awful.button({ }, 3, function () awful.layout.inc(-1) end),
-    awful.button({ }, 4, function () awful.layout.inc( 1) end),
-    awful.button({ }, 5, function () awful.layout.inc(-1) end)
-  ))
+	-- Create an imagebox widget which will contain an icon indicating which layout we're using.
+	-- We need one layoutbox per screen.
+	s.mylayoutbox = awful.widget.layoutbox(s)
+	s.mylayoutbox:buttons(gears.table.join(
+		awful.button({}, 1, function()
+			awful.layout.inc(1)
+		end),
+		awful.button({}, 3, function()
+			awful.layout.inc(-1)
+		end),
+		awful.button({}, 4, function()
+			awful.layout.inc(1)
+		end),
+		awful.button({}, 5, function()
+			awful.layout.inc(-1)
+		end)
+	))
 
-  -- Create a taglist widget
-  s.mytaglist = awful.widget.taglist {
-    screen  = s,
-    filter  = awful.widget.taglist.filter.all,
-    buttons = taglist_buttons,
-    style = {
-      spacing = 5,
-    },
-  }
+	-- Create a taglist widget
+	s.mytaglist = awful.widget.taglist({
+		screen = s,
+		filter = awful.widget.taglist.filter.all,
+		buttons = taglist_buttons,
+		style = {
+			spacing = 5,
+		},
+	})
 
-  -- Create a tasklist widget
-  s.mytasklist = awful.widget.tasklist {
-    screen  = s,
-    filter  = awful.widget.tasklist.filter.currenttags,
-    buttons = tasklist_buttons,
-    style = {
-      spacing = 15,
-    },
-    layout   = {
-        spacing_widget = {
-            {
-                forced_width  = 5,
-                forced_height = 24,
-                thickness     = 1,
-                color         = '#cacaca',
-                widget        = wibox.widget.separator
-            },
-            valign = 'center',
-            halign = 'center',
-            widget = wibox.container.place,
-        },
-        spacing = 1,
-        layout  = wibox.layout.fixed.horizontal
-    },
-    -- Notice that there is *NO* wibox.wibox prefix, it is a template,
-    -- not a widget instance.
-    widget_template = {
-        {
-            wibox.widget.base.make_widget(),
-            forced_height = 5,
-            id            = 'background_role',
-            widget        = wibox.container.background,
-        },
-        {
-            {
-                id     = 'clienticon',
-                widget = awful.widget.clienticon,
-            },
-            margins = 2,
-            widget  = wibox.container.margin
-        },
-        nil,
-        create_callback = function(self, c, index, objects) --luacheck: no unused args
-            self:get_children_by_id('clienticon')[1].client = c
-        end,
-        layout = wibox.layout.align.vertical,
-    },
-    placement = awful.placement.center,
-  }
+	-- Create a tasklist widget
+	s.mytasklist = awful.widget.tasklist({
+		screen = s,
+		filter = awful.widget.tasklist.filter.currenttags,
+		buttons = tasklist_buttons,
+		style = {
+			spacing = 15,
+		},
+		layout = {
+			spacing_widget = {
+				{
+					forced_width = 5,
+					forced_height = 24,
+					thickness = 1,
+					color = "#cacaca",
+					widget = wibox.widget.separator,
+				},
+				valign = "center",
+				halign = "center",
+				widget = wibox.container.place,
+			},
+			spacing = 1,
+			layout = wibox.layout.fixed.horizontal,
+		},
+		-- Notice that there is *NO* wibox.wibox prefix, it is a template,
+		-- not a widget instance.
+		widget_template = {
+			{
+				wibox.widget.base.make_widget(),
+				forced_height = 5,
+				id = "background_role",
+				widget = wibox.container.background,
+			},
+			{
+				{
+					id = "clienticon",
+					widget = awful.widget.clienticon,
+				},
+				margins = 2,
+				widget = wibox.container.margin,
+			},
+			nil,
+			create_callback = function(self, c, index, objects) --luacheck: no unused args
+				self:get_children_by_id("clienticon")[1].client = c
+			end,
+			layout = wibox.layout.align.vertical,
+		},
+		placement = awful.placement.center,
+	})
 
-  -- Create the wibox
-  s.mywibox = awful.wibar({ position = "top", screen = s })
+	-- Create the wibox
+	s.mywibox = awful.wibar({ position = "top", screen = s })
 
-  -- Add widgets to the wibox
-  s.mywibox:setup {
-    layout = wibox.layout.align.horizontal,
-    { -- Left widgets
-      layout = wibox.layout.fixed.horizontal,
-      RC.launcher,
-      s.mytaglist,
-      s.mypromptbox,
-    },
-    s.mytasklist, -- Middle widget
-    { -- Right widgets
-      layout = wibox.layout.fixed.horizontal,
-      mykeyboardlayout,
-      wibox.widget.systray(),
-      mytextclock,
-      s.mylayoutbox,
-    },
-  }
+	-- Add widgets to the wibox
+	s.mywibox:setup({
+		layout = wibox.layout.align.horizontal,
+		{ -- Left widgets
+			layout = wibox.layout.fixed.horizontal,
+			RC.launcher,
+			s.mytaglist,
+			s.mypromptbox,
+		},
+		s.mytasklist, -- Middle widget
+		{ -- Right widgets
+			layout = wibox.layout.fixed.horizontal,
+			-- mykeyboardlayout,
+			wibox.widget.systray(),
+			mytextclock,
+			s.mylayoutbox,
+		},
+	})
 end)
 -- }}}
-
